98fecbbde8c7ac3bb6eb1ee971f202adcc0f4c36
Kyle McNally <kyle@kmcnally.net>
2017-05-11 15:23:13 -0400

    Expanded unwatcher to let the user choose the events

--- folder-unwatcher.ps1
+++ folder-unwatcher.ps1
@@ -1,3 +1,27 @@
-Unregister-Event FileDeleted
-Unregister-Event FileCreated
-Unregister-Event FileChanged
+$selection = Get-EventSubscriber
+
+if($selection.Count -gt 1){
+    $title = "Event Selection"
+    $message = "Which event would you like to unsubscribe from?"
+
+    $choices = @()
+    for($index = 0; $index -lt $selection.Count; $index++){
+        $choices += New-Object System.Management.Automation.Host.ChoiceDescription ($selection[$index].SourceIdentifier),($selection[$index].SourceIdentifier)
+    }
+    $choices += New-Object System.Management.Automation.Host.ChoiceDescription "All","Delete All Events"
+
+    $options = [System.Management.Automation.Host.ChoiceDescription[]]$choices
+    $result = $host.UI.PromptForChoice($title, $message, $options, 0)
+
+    if($result -eq $selection.count){
+        $selection = $selection
+    } else {
+        $selection = $selection[$result]
+    }
+
+}
+
+foreach($evt in $selection){
+    Write-Host "$evt.SourceIdentifier removed"
+    Unregister-Event $evt.SourceIdentifier
+}
